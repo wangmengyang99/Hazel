182be4f4f66be1f79f0f4fa5d2ad7f1b1c9f8bb0
wmy <2137633492@qq.com>
2023-05-06 14:42:02 +0800

    add premake

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -1,6 +1,6 @@
 #pragma once
 #ifdef HZ_PLATFORM_WINOWS
-	#ifdef HZ_BUGLD_DLL
+	#ifdef HZ_BUILD_DLL
 		#define HAZEL_API __declspec(dllexport)
 	#else
 		#define HAZEL_API __declspec(dllimport)
--- premake5.lua
+++ premake5.lua
@@ -1,95 +1,106 @@
 workspace "Hazel"
-    architecture "x64"
+	architecture "x64"
 
-    configurations
-    {
-        "Debug",
-        "Release",
-        "Dist"
-    }
-    
-    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
 project "Hazel"
-    location "Hazel"
-    kind "SharedLib"
-    language "C++"
+	location "Hazel"
+	kind "SharedLib"
+	language "C++"
 
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
-    includedirs{
-        "%{prj.name}/vendor/spdlog/include"
-    }
-    filter "system:windows"
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/include",
+		"%{prj.name}/src"
+	}
+
+	filter "system:windows"
 		cppdialect "C++17"
 		staticruntime "On"
 		systemversion "latest"
- 
-   defines 
-    {
-        " HZ_PLATFORM_WINDOW",
-        "HZ_DEBUG_DLL"
-    }
-  postbuildcommands
-   {
-	("{COPY} %(cfg.buildtarget.relpath)\" ../bin/" .. outputdir .. "/Sandbox\"")
-   }
 
-    filter "configurations:Debug"
-    defines "HZ_DEBUG"
-    symbols "On"
- 
-    filter "configurations:Release"
-    defines "HZ_RELEASE"
-    symbols "On"
- 
-    filter "configurations:Dist"
-    defines "HZ_DIST"
-    symbols "On"
+		defines
+		{
+			"HZ_PLATFORM_WINOWS",
+			"HZ_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/sandbox\"")
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
+
 project "sandbox"
-    location "sandbox"
-    kind "ConsoleApp"
-    language "C++"
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	location "sandbox"
+	kind "ConsoleApp"
+	language "C++"
 
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
-    defines
-    {
-     "HZ_PLATFORM_WINOWS"
-    }
-    links
-    {
-        "Hazel"
-    }
-    includedirs
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
 	{
-        "Hazel/vendor/spdlog/include",
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Hazel/vendor/include",
 		"Hazel/src"
 	}
 
-    filter "system:windows"
+	links
+	{
+		"Hazel"
+	}
+
+	filter "system:windows"
 		cppdialect "C++17"
 		staticruntime "On"
 		systemversion "latest"
 
-        filter "configurations:Debug"
-        defines "HZ_DEBUG"
-        symbols "On"
-     
-        filter "configurations:Release"
-        defines "HZ_RELEASE"
-        symbols "On"
-     
-        filter "configurations:Dist"
-        defines "HZ_DIST"
-        symbols "On"
+		defines
+		{
+			"HZ_PLATFORM_WINOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
